1fdf61122dc66ecb7ef988d63487d729749552f6
Kartavya1905 <kartavyamandora648@gmail.com>
2025-02-14 21:07:58 -0500

    random function code

---
+++ Sample.py
@@ -0,0 +1,2 @@
+values = {float(9.0),int(9)} # set values as {("float",9.0),("int",9)} or {"9",9.0} or {9,"9.0"}
+print(values)
---
+++ random_code.py
@@ -0,0 +1,10 @@
+#import random method 
+import random
+
+# Generates a specific seed  
+random.seed(44)
+ 
+#Generates a random integer between 1, 10
+print(random.randint(1, 10)) 
+print(random.randint(1, 10)) 
+print(random.randint(1, 10))
